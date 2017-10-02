b384b9500bd560a4422fcaeeafe14108e3475bef
Clay Tinnell <ctinnell@gaig.com>
2017-10-02 09:31:32 -0400

    Initial Commit

---
+++ PostcardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostcardDemo
+//
+//  Created by Tinnell, Clay on 10/2/17.
+//  Copyright © 2017 Tinnell, Clay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
